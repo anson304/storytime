18ea5d64d33864a9dd9552fd6db68f7ac76eb4e0
Anson <u7139215@anu.edu.au>
2024-06-09 00:09:01 +1000

    add restart button, fix bugs

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -1,13 +1,18 @@
 // src/components/CharacterPage.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getRandomSubset, N_OPTIONS} from '../Util';
 
 function CharacterPage({ ideas, selectedIdeas, toggleIdea, onNext }) {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    setCharacters(getRandomSubset(ideas,N_OPTIONS));
+  }, []);
   return (
     <div className="character-page">
       <h2>Choose your character...</h2>
       <div className="ideas-container">
-        {ideas.map((idea, index) => (
+        {characters.map((idea, index) => (
           <div
             key={index}
             className={`idea-tile ${selectedIdeas.includes(idea) ? 'selected' : ''}`}
